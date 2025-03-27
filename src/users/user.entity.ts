04db03b0efb10d968223a16d34078b6a9c17f302
Ahmed Mahmud <ahmedmahmud1994@hotmail.com>
2025-03-27 05:45:26 +0000

    created the user role enum, updated the user dto and user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,21 +1,46 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { RefreshToken } from '../auth/entities/refresh-token.entity';
 import { Exclude } from 'class-transformer';
+import { Role } from 'src/common/enums/Role.enum';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  name: string;
+
   @Column({ unique: true })
   email: string;
 
   @Column()
-  name: string;
-
-  @Column()
   @Exclude()
   password: string;
+
+  @Column({
+    type: 'enum',
+    nullable: false,
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
+
+  @CreateDateColumn(
+    {
+      type: 'timestamptz',
+      default: () => 'CURRENT_TIMESTAMP',
+    },
+  )
+  createdAt: Date;
+
+  @UpdateDateColumn(
+    {
+      type: 'timestamptz',
+      default: () => 'CURRENT_TIMESTAMP',
+    },
+  )
+  updatedAt: Date;
 
   @OneToMany(() => RefreshToken, (refreshToken) => refreshToken.user, {
     cascade: true,
