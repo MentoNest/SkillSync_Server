215e1aeaf1b7ac6fe5b0c376badd7b3b050896ac
ahmad <abdulkareemahmad413@gmail.com>
2025-04-23 15:39:13 +0100

    initial setUp for the project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { GlobalExceptionsFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,13 +9,11 @@ async function bootstrap() {
     .setTitle('SkillSync API')
     .setDescription('API documentation for SkillSync backend')
     .setVersion('1.0')
-    .addBearerAuth() // Adds JWT authentication support
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  app.useGlobalFilters(new GlobalExceptionsFilter());
 
   await app.listen(3000);
 }
