2200d7956f5ae4186cdde5c51ca2f5c2f871af26
shamoo53 <aliyushamsu16@gmail.com>
2026-01-22 09:51:00 +0100

    Skills-Taxonomy-Mentor-Skill-Linking
    Skills-Taxonomy-Mentor-Skill-Linking

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { DatabaseModule } from './database/database.module';
+import { SkillModule } from './modules/skill.module';
 
 @Module({
-  imports: [DatabaseModule, HealthModule],
+  imports: [DatabaseModule, HealthModule, SkillModule],
   controllers: [AppController],
   providers: [AppService],
 })
