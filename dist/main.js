748ce38a6310bd95dadade6fb6479a5efcb0e915
Nabeelahh <saninabeelah@gmail.com>
2026-04-22 18:54:10 +0100

    jjj

---
+++ dist/main.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const swagger_1 = require("@nestjs/swagger");
+const helmet_1 = require("helmet");
+const compression = require("compression");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        logger: ['error', 'warn', 'log', 'debug'],
+    });
+    const configService = app.get(config_1.ConfigService);
+    app.use((0, helmet_1.default)());
+    app.use(compression());
+    app.enableCors({
+        origin: configService.get('CORS_ORIGIN') || '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
+    }));
+    app.setGlobalPrefix(configService.get('API_PREFIX') || 'api');
+    if (configService.get('NODE_ENV') !== 'production') {
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle('SkillSync API')
+            .setDescription('Enterprise-level API for SkillSync platform')
+            .setVersion('1.0')
+            .addTag('api')
+            .build();
+        const document = swagger_1.SwaggerModule.createDocument(app, config);
+        swagger_1.SwaggerModule.setup('api/docs', app, document);
+    }
+    const port = configService.get('PORT') || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}`);
+    console.log(`API documentation available at: http://localhost:${port}/api/docs`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
