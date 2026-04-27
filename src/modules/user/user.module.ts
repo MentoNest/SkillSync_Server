d2c72290e23599a53f5f40061dc045488d71e819
ummarig <nafiuigee@gmail.com>
2026-04-27 06:11:23 +0000

    implemeneted the featured mentord

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -8,15 +8,19 @@ import { MenteeProfile } from './entities/mentee-profile.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { ScheduledCleanupService } from './services/scheduled-cleanup.service';
+import { MentorAdminService } from './services/mentor-admin.service';
+import { MentorService } from './services/mentor.service';
 import { AuthModule } from '../auth/auth.module';
+import { MentorAdminController } from './controllers/mentor-admin.controller';
+import { MentorController } from './controllers/mentor.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, MentorProfile, MenteeProfile, AuditLog, RefreshToken]),
     AuthModule,
   ],
-  controllers: [UserController],
-  providers: [UserService, ScheduledCleanupService],
-  exports: [UserService],
+  controllers: [UserController, MentorAdminController, MentorController],
+  providers: [UserService, ScheduledCleanupService, MentorAdminService, MentorService],
+  exports: [UserService, MentorAdminService, MentorService],
 })
 export class UserModule {}
